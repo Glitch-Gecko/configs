6ed0b1c410dd0c591b0dda6dc9deb432dd903057
Glitch-Gecko <nvformella@gmail.com>
2024-02-26 23:06:50 -0600

    Improved startup

--- dotfiles/hypr/cleanup_after_start.sh
+++ dotfiles/hypr/cleanup_after_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sleep $1
+swww init & swww img /home/nicolas/.config/hypr/wallpapers/$2 && echo $wallpaper > /tmp/current_wall
 hyprctl keyword windowrule "workspace unset,firefox"
 sleep 1
 hyprctl keyword windowrule "workspace unset,kitty"
